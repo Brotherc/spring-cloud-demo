60455670a74b3c4b2c2b0c43fec7640399d47d78
Abel.cs.liu <Abel.cs.lua@teleone.cn>
2020-03-18 16:04:59 +0800

    swagger

--- ribbon/src/main/java/com/brotherc/ribbon/RibbonApplication.java
+++ ribbon/src/main/java/com/brotherc/ribbon/RibbonApplication.java
@@ -4,9 +4,11 @@ import com.brotherc.ribbon.config.NewRuleConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @RibbonClient(name = "provider", configuration = NewRuleConfig.class)
+@EnableSwagger2
 public class RibbonApplication {
 
   public static void main(String[] args) {
