16e8ffbcf4b872ffc4353cbcc170cf95b146b955
Abel.cs.liu <Abel.cs.lua@teleone.cn>
2020-03-18 16:10:56 +0800

    sleuth、zipkin

---
+++ sleuth-zipkin/src/test/java/com/brotherc/sleuthzipkin/SleuthZipkinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherc.sleuthzipkin;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthZipkinApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
---
+++ sleuth/src/test/java/com/brotherc/sleuth/SleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherc.sleuth;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
