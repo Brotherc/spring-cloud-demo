48c4bf15b865e175af5da0ae556119a19bea0b79
Abel.cs.liu <Abel.cs.lua@teleone.cn>
2020-03-18 16:09:22 +0800

    swagger

--- service-consumer/src/main/java/com/brotherc/demo/ServiceConsumerApplication.java
+++ service-consumer/src/main/java/com/brotherc/demo/ServiceConsumerApplication.java
@@ -3,9 +3,11 @@ package com.brotherc.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class ServiceConsumerApplication {
 
   public static void main(String[] args) {
